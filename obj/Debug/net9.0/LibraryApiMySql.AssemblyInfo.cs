7fadf53122a2b6e9421df16deccf50ca6be4c464
Rashedul Alam <rashed464@outlook.com>
2025-10-05 17:09:06 +0600

    code updated

--- obj/Debug/net9.0/LibraryApiMySql.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApiMySql.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApiMySql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca7cc540523208723cdd2dd428de16d5e4ae977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6b2cafc575cb156a262910a46c1a95c55e86bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApiMySql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApiMySql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
